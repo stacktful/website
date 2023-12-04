d961169db4536fcb8c87da9075c354351feeadbb
thibaut-ebitwise <thibaut@ebitwise.nl>
2023-12-04 17:46:57 +0000

    add contact options to Contact section

--- website/src/components/Contact.js
+++ website/src/components/Contact.js
@@ -1,7 +1,50 @@
 import Section from "./Section";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+
+// const contacts = [
+//   {
+//     icon: "IoMailOutline",
+//     text: "",
+//     link: "",
+//   },
+//   {
+//     icon: "IoLogoGithub",
+//     text: "",
+//     link: "",
+//   },
+//   {
+//     icon: "IoLogoLinkedin",
+//     text: "",
+//     link: "",
+//   },
+//   {
+//     icon: "IoLogoInstagram",
+//     text: "",
+//     link: "",
+//   },
+// ];
 
 function Contact() {
-  return <Section title="Contact"></Section>;
+  return (
+    <Section title="Contact">
+      <div className="contact-list">
+        <div className="contact-item">
+          <MdOutlineAlternateEmail />
+          Email
+        </div>
+        <div className="contact-item">
+          <FaLinkedin />
+          LinkedIn
+        </div>
+        <div className="contact-item">
+          <IoLogoGithub />
+          GitHub
+        </div>
+      </div>
+    </Section>
+  );
 }
 
 export default Contact;
