f7887cba8c81ac6601f69751f7d2ef66fafa8824
thibaut-stacktful <thibaut@stacktful.com>
2023-12-12 17:52:35 +0000

    styling(15)

--- website/src/components/Section.js
+++ website/src/components/Section.js
@@ -5,7 +5,10 @@ function Section({ children, ...props }) {
         <section>
           <div className="section-header">
             <div className="section-header-line"></div>
-            <h2 className="section-header-title">{props.title}</h2>
+            <h2 className="section-header-title">
+              <span className="section-header-title-index">{`${"01"}.`}</span>
+              <span className="section-header-title-text">{props.title}</span>
+            </h2>
             <div className="section-header-line"></div>
           </div>
           {children}
