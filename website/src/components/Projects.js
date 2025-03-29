d7110828cede2435aaca5235f44be8701052b52f
stacktful <thibaut@stacktful.com>
2025-03-29 17:34:05 +0100

    add registered trademark symbol to project

--- website/src/components/Projects.js
+++ website/src/components/Projects.js
@@ -7,19 +7,19 @@ import imgAvonic from "../static/img/project-avonic.png";
 
 const projects = [
   {
-    title: "Dashwise",
+    title: <>Dashwise&reg;</>,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum, \
-      eros nec commodo cursus, nunc elit tincidunt nunc, et tincidunt nunc elit in \
-      nulla. Donec euismod, nunc sed ultricies tincidunt, felis nunc tincidunt nisi, \
-      et aliquam felis arcu eget mauris. Donec ultricies, lorem eget lacinia aliquet, \
-      nisl dui sollicitudin risus, vitae ultricies justo nisl eu nunc. In hac habitasse \
-      platea dictumst. In hac habitasse platea dictumst.",
+    eros nec commodo cursus, nunc elit tincidunt nunc, et tincidunt nunc elit in \
+    nulla. Donec euismod, nunc sed ultricies tincidunt, felis nunc tincidunt nisi, \
+    et aliquam felis arcu eget mauris. Donec ultricies, lorem eget lacinia aliquet, \
+    nisl dui sollicitudin risus, vitae ultricies justo nisl eu nunc. In hac habitasse \
+    platea dictumst. In hac habitasse platea dictumst.",
     link: "https://dashwise.io",
     image: imgEbitwise,
   },
   {
-    title: "CamDirector",
+    title: <>CamDirector&reg;</>,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum, \
       eros nec commodo cursus, nunc elit tincidunt nunc, et tincidunt nunc elit in \
