554b53e83e38ed818fbaa6cdd75beaf5a94c1c76
stacktful <thibaut@stacktful.com>
2025-03-31 22:02:07 +0200

    update content of 'Experience' section for better fit

--- website/src/data/hero.js
+++ website/src/data/hero.js
@@ -1,25 +1,19 @@
 const startDate = new Date(2018, 5, 1);
 const role = "Fullstack Software Engineer";
 
-function monthDiff(startDate) {
+function yearsSince(date) {
   const today = new Date();
-  return (
-    today.getMonth() -
-    startDate.getMonth() +
-    12 * (today.getFullYear() - startDate.getFullYear())
-  );
+  return Math.floor((today - date) / (1000 * 60 * 60 * 24 * 365));
 }
-
-const yearsOfExp = Math.floor(monthDiff(startDate) / 12);
 
 const summary = (
   <>
-    {role} with over {yearsOfExp} years of experience in Python-based greenfield
-    projects, with MSc background in engineering, robotics & AI. Specialised in
-    end-to-end development of cloud-native web applications (B2B, SaaS), in
-    Agile/Scrum multidisciplinary teams within start-ups and international
-    corporations. Strong advocate of clean code, automated workflows and clear
-    communication for optimal collaboration.
+    {role} with over {yearsSince(startDate)} years of experience in Python-based
+    greenfield projects, with MSc background in engineering, robotics & AI.
+    Specialised in end-to-end development of cloud-native web applications (B2B,
+    SaaS), in Agile/Scrum multidisciplinary teams within start-ups and
+    international corporations. Strong advocate of clean code, automated
+    workflows and clear communication for optimal collaboration.
   </>
 );
 
