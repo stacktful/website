eb63e178faeb7796b8f6850049183adc7915a2de
stacktful <thibaut@stacktful.com>
2025-04-04 11:15:47 +0200

    content: add 'Cloud Infrastructure' service to the list"

--- website/src/data/services.js
+++ website/src/data/services.js
@@ -1,44 +1,33 @@
 import { AiOutlineCode } from "react-icons/ai";
 import { IoCodeSlash } from "react-icons/io5";
 import { GoGitCompare } from "react-icons/go";
+import { TbCloudComputing } from "react-icons/tb";
 
 export const services = [
   {
     icon: <IoCodeSlash />,
-    title: "Frontend Development",
+    title: "Web Development",
     description:
       "I build responsive user interfaces using the latest technologies and best practices.",
-    techStack: ["React", "Node.js", "JavaScript", "HTML", "CSS"],
+    techStack: ["React", "TypeScript", "JavaScript", "HTML", "CSS"],
   },
   {
     icon: <AiOutlineCode />,
-    title: "Backend Development",
-    description:
-      "I build web applications using the latest technologies and best practices.",
-    techStack: [
-      "Python",
-      "Flask",
-      "FastAPI",
-      "Django",
-      "PostgreSQL",
-      "MongoDB",
-    ],
+    title: "Software Engineering",
+    description: "I build Python-based API's using the latest technologies and best practices.",
+    techStack: ["Python", "Flask", "FastAPI", "Django", "PostgreSQL", "MongoDB"],
   },
   {
     icon: <GoGitCompare />,
     title: "DevOps",
-    description:
-      "I build web applications using the latest technologies and best practices.",
-    techStack: [
-      "Bash",
-      "Git",
-      "GitHub Actions",
-      "Docker",
-      "Terraform",
-      "GCP",
-      "Azure",
-      "AWS",
-    ],
+    description: "I build web applications using the latest technologies and best practices.",
+    techStack: ["Terraform", "GitHub Actions", "Docker"],
+  },
+  {
+    icon: <TbCloudComputing />,
+    title: "Cloud Infrastructure",
+    description: "I build web applications using the latest technologies and best practices.",
+    techStack: ["AWS", "Google Cloud", "Azure"],
   },
 ];
 
